10a7c99cfab78e4f7bcaf880bde0647a08ce6943
kcyuen812 <63786046+kcyuen812@users.noreply.github.com>
2023-05-01 01:25:08 +0800

    Add files via upload

--- pages/6_Trade_BTC.py
+++ pages/6_Trade_BTC.py
@@ -69,8 +69,6 @@ elif (
 
 
     # Initialize connection.
-    # Uses st.cache_resource to only run once.
-    @st.cache_resource
     def init_connection():
         return psycopg2.connect(
             host=Settings().PGHOST,
