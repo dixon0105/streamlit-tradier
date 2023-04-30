8741ba085810216abcb516afbb172da09918aa7d
kcyuen812 <63786046+kcyuen812@users.noreply.github.com>
2023-04-30 22:21:49 +0800

    Show prices in USD

--- pages/1_Get_Crypto_Prices.py
+++ pages/1_Get_Crypto_Prices.py
@@ -45,12 +45,11 @@ elif (
     # Actual link: 'https://pro-api.coinmarketcap.com/v2/cryptocurrency/quotes/latest'
     # Sandbox link: 'https://sandbox-api.coinmarketcap.com/v2/cryptocurrency/quotes/latest'
     # Below 2 lines for finding the IDs of corresponding coins only
-    url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/map'
-    parameters = {'symbol':'BTC,ETH,LINK,USDT,SHIB,LTC'}
-
-    # url = "https://sandbox-api.coinmarketcap.com/v2/cryptocurrency/quotes/latest"
-    # parameters = {"convert": "USD", "id": "825,1,1027,1975,5994"}
-    # ID: 1 (BTC), 1027 (ETH), 1975 (LINK), 5994 (SHIB), 825 (USDT)
+    # url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/map'
+    # parameters = {'symbol':'BTC,ETH,LINK,USDT,SHIB,LTC'}
+    url = "https://pro-api.coinmarketcap.com/v2/cryptocurrency/quotes/latest"
+    parameters = {"convert": "USD", "id": "1, 1027, 1975, 825, 2"}
+    # ID: 1 (BTC), 1027 (ETH), 1975 (LINK), 825 (USDT), 2 (LTC), 5994 (SHIB)
 
     headers = {
         "Accepts": "application/json",
@@ -62,10 +61,8 @@ elif (
     try:
         response = session.get(url, params=parameters)
         data = json.loads(response.text)
-        st.write(data)
-        #for i in data["data"]:
-        #    st.write(f'Price of {data["data"][i]["name"]} ({data["data"][i]["symbol"]}) in USD: {round(data["data"][i]["quote"]["USD"]["price"],4)}')
-        #    st.write(f'Price of {data["data"][i]["name"]} ({data["data"][i]["symbol"]}) in USDT: {round(data["data"][i]["quote"]["USDT"]["price"], 4)}')
+        for i in data["data"]:
+            st.write(f'Price of {data["data"][i]["name"]} ({data["data"][i]["symbol"]}) in USD: {round(data["data"][i]["quote"]["USD"]["price"], 4)}')
 
     except (ConnectionError, Timeout, TooManyRedirects) as e:
         st.write(e)
